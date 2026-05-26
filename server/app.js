18a3b0dcd103ea9f03c0eb7078d3c0df67fdcd0b
davidfrmn <furmandavid@proton.me>
2026-05-26 14:02:03 +0300

    todo and task api

--- server/app.js
+++ server/app.js
@@ -1,76 +1,22 @@
 import { Hono } from "@hono/hono";
-import { cors } from "@hono/hono/cors";
-import { logger } from "@hono/hono/logger";
-import postgres from "postgres";
+import * as todoController from "./todoController.js";
+import * as taskController from "./taskController.js";
 
 const app = new Hono();
-const sql = postgres();
 
-app.use("/*", cors());
-app.use("/*", logger());
 
-let visits = 0;
-app.get("/api/visits", (c) => {
-  visits++;
-  return c.json({ visits });
-});
+app.post("/api/todos", todoController.create);
+app.get("/api/todos", todoController.readAll);
+app.get("/api/todos/:todoId", todoController.readOne);
+app.put("/api/todos/:todoId", todoController.update);
+app.delete("/api/todos/:todoId", todoController.deleteOne);
 
-// retrieving todos from database on requests to /api/todos
-app.get("/api/todos", async (c) => {
-  const todos = await sql`SELECT * FROM todos`;
-  return c.json(todos);
-});
 
-app.get("/api/todos/:todoId", async (c) => {
-    const todoId = Number(c.req.param("todoId"));
-    if (!Number.isInteger(todoId)) {
-        return c.json({error: "Invalid todo id"}, 400)
-    }
-    
-    const result = await sql`SELECT * FROM todos where id = ${todoId}`;
-    
-    if (result.length === 0) {
-        return c.json({error: "Todo not found"}, 404);
-    }
-    return c.json(result[0]);
-});
+app.post("/api/todos/:todoId/tasks", taskController.create);
+app.get("/api/todos/:todoId/tasks", taskController.readAll);
+app.get("/api/todos/:todoId/tasks/:taskId", taskController.readOne);
+app.put("/api/todos/:todoId/tasks/:taskId", taskController.update);
+app.delete("/api/todos/:todoId/tasks/:taskId", taskController.deleteOne);
 
-app.get("/api/books", async (c) => {
-  const result = await sql`SELECT * FROM books`;
-  return c.json(result);
-});
-
-app.get("/api/books/:bookId", async (c) => {
-  const id = Number(c.req.param("bookId"));
-  if (!Number.isInteger(id)) {
-    return c.json({ error: "Invalid book id" }, 400);
-  }
-
-  const result = await sql`SELECT *
-    FROM books
-    WHERE id = ${id}`;
-  if (result.length === 0) {
-    return c.json({ error: "Book not found" }, 404);
-  }
-  return c.json(result[0]);
-});
-
-app.post("/api/books", async (c) => {
-  const book = await c.req.json();
-
-  if (!book.title ||
-    !book.description ||
-    !book.published_at ||
-    !book.page_count) {
-    return c.json({ error: "Missing required fields" }, 400);
-  }
-
-  const result = await sql`INSERT INTO books
-    (title, description, published_at, page_count)
-    VALUES (${book.title}, ${book.description}, ${book.published_at}, ${book.page_count})
-    RETURNING *;`;
-
-  return c.json(result[0], 201);
-});
 
 export default app;
---
+++ server/taskController.js
@@ -0,0 +1,98 @@
+import * as taskRepository from './taskRepository.js';
+
+// add controller functions here and export them
+
+const create = async (c) => {
+  const todoId = Number(c.req.param("todoId"));
+  if (!Number.isInteger(todoId)) {
+    return c.json({error: "Invalid todo id"}, 400);
+  }
+
+  const task = await c.req.json();
+  if (!task.description) {
+    return c.json({ error: "Missing required fields" }, 400);
+  }
+
+  const newTask = await taskRepository.create(todoId, task);
+  return c.json(newTask,201);
+};
+
+const readAll = async (c) => {
+  const todoId = Number(c.req.param("todoId"));
+  if (!Number.isInteger(todoId)) {
+    return c.json({error: "Invalid todo id"}, 400);
+  }
+
+  const tasks = await taskRepository.findAll(todoId);
+  return c.json(tasks);
+};
+
+const readOne = async (c) => {
+  /*const todoId = Number(c.req.param("todoId"));
+  if (!Number.isInteger(todoId)) {
+    return c.json({ error: "Invalid todo id" }, 400);
+  }
+  */
+
+  const taskId = Number(c.req.param("taskId"));
+  if (!Number.isInteger(taskId)) {
+    return c.json({ error: "Invalid task id" }, 400);
+  }
+  const task = await taskRepository.findById(taskId);
+
+  if (!task) {
+    return c.json({ error: "Task not found" }, 404);
+  }
+
+  return c.json(task);
+};
+
+const update = async (c) => {
+
+  const taskId = Number(c.req.param("taskId"));
+  if (!Number.isInteger(taskId)) {
+    return c.json({ error: "Invalid task id" }, 400);
+  }
+  /*
+  const todoId = Number(c.req.param("todoId"));
+  if (!Number.isInteger(todoId)) {
+    return c.json({ error: "Invalid todo id" }, 400);
+  }
+  */
+
+  const task = await c.req.json();
+  if (task.is_done == undefined || !task.description) {
+    return c.json({ error: "Missing required fields" }, 400);
+  }
+
+  const updatedTask = await taskRepository.updateById(taskId, task);
+
+  if (!updatedTask) {
+    return c.json({ error: "Task not found" }, 404);
+  }
+
+  return c.json(updatedTask);
+};
+
+const deleteOne = async (c) => {
+  /*
+  const todoId = Number(c.req.param("todoId"));
+  if (!Number.isInteger(todoId)) {
+    return c.json({ error: "Invalid todo id" }, 400);
+  }
+  */
+  const taskId = Number(c.req.param("taskId"));
+  if (!Number.isInteger(taskId)) {
+    return c.json({ error: "Invalid task id" }, 400);
+  }
+
+  const deletedTask = await taskRepository.deleteById(taskId);
+
+  if (!deletedTask) {
+    return c.json({ error: "Task not found" }, 404);
+  }
+
+  return c.json(deletedTask);
+};
+
+export { create, deleteOne, readAll, readOne, update };
