053dfbe1e8163d4be823ed46e8ab524986e0c83e
davidfrmn <furmandavid@proton.me>
2026-06-05 11:06:39 +0200

    client side user management

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { useAuthState } from "$lib/states/authState.svelte.js";
+  let { children } = $props();
+  const authState = useAuthState();
+</script>
+
+<header>
+  {#if authState.user}
+    <div>
+      <span>Hello, {authState.user.email}!</span>
+      <button onclick={() => authState.logout()}>Logout</button>
+    </div>
+  {:else}
+    <ul>
+      <li><a href="/auth/login">Login</a></li>
+      <li><a href="/auth/register">Register</a></li>
+    </ul>
+  {/if}
+</header>
+
+<main>
+  {@render children()}
+</main>
